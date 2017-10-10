ccbe838f98c85e930835ca6e0d4c19606d2909c6
zxhnext <xiaohannext@qq.com>
2017-10-10 11:32:32 +0800

    first commit

---
+++ js/waterfall.js
@@ -0,0 +1,109 @@
+/* jshint asi:true */
+//先等图片都加载完成
+//再执行布局函数
+
+/**
+ * 执行主函数
+ * @param  {[type]} function( [description]
+ * @return {[type]}           [description]
+ */
+(function() {
+
+  /**
+     * 内容JSON
+     */
+  var demoContent = [
+    {
+      demo_link: 'http://gaohaoyang.github.io/test/headerTransition/',
+      img_link: 'https://ooo.0o0.ooo/2016/06/20/5768c1597d1fe.png',
+      code_link: 'https://github.com/Gaohaoyang/test/tree/master/headerTransition',
+      title: 'Header Transition 渐变动画',
+      core_tech: 'jQuery BootStrap CSS3',
+      description: '花费不到半小时帮师兄做了一个简单的 CSS3 动画效果，当页面滚动到指定距离时，header 区的背景色由透明变为蓝色。仿照了网站 <a href ="https://quorrajs.org/">https://quorrajs.org/</a> 的 Header 区动画效果。'
+    }, {
+      demo_link: 'http://gaohaoyang.github.io/test/bootstrap-zhihu/',
+      img_link: 'http://7q5cdt.com1.z0.glb.clouddn.com/teach-girlfriend-html-CopyZhihu.jpg',
+      code_link: 'https://github.com/Gaohaoyang/test/tree/master/bootstrap-zhihu',
+      title: '仿知乎页面',
+      core_tech: 'HTML BootStrap',
+      description: '使用BootStrap仿照知乎做了一个静态页面。'
+    }
+  ];
+
+  contentInit(demoContent) //内容初始化
+  waitImgsLoad() //等待图片加载，并执行布局初始化
+}());
+
+/**
+ * 内容初始化
+ * @return {[type]} [description]
+ */
+function contentInit(content) {
+  // var htmlArr = [];
+  // for (var i = 0; i < content.length; i++) {
+  //     htmlArr.push('<div class="grid-item">')
+  //     htmlArr.push('<a class="a-img" href="'+content[i].demo_link+'">')
+  //     htmlArr.push('<img src="'+content[i].img_link+'">')
+  //     htmlArr.push('</a>')
+  //     htmlArr.push('<h3 class="demo-title">')
+  //     htmlArr.push('<a href="'+content[i].demo_link+'">'+content[i].title+'</a>')
+  //     htmlArr.push('</h3>')
+  //     htmlArr.push('<p>主要技术：'+content[i].core_tech+'</p>')
+  //     htmlArr.push('<p>'+content[i].description)
+  //     htmlArr.push('<a href="'+content[i].code_link+'">源代码 <i class="fa fa-code" aria-hidden="true"></i></a>')
+  //     htmlArr.push('</p>')
+  //     htmlArr.push('</div>')
+  // }
+  // var htmlStr = htmlArr.join('')
+  var htmlStr = ''
+  for (var i = 0; i < content.length; i++) {
+    htmlStr += '<div class="grid-item">' + '   <a class="a-img" href="' + content[i].demo_link + '">' + '       <img src="' + content[i].img_link + '">' + '   </a>' + '   <h3 class="demo-title">' + '       <a href="' + content[i].demo_link + '">' + content[i].title + '</a>' + '   </h3>' + '   <p>主要技术：' + content[i].core_tech + '</p>' + '   <p>' + content[i].description + '       <a href="' + content[i].code_link + '">源代码 <i class="fa fa-code" aria-hidden="true"></i></a>' + '   </p>' + '</div>'
+  }
+  var grid = document.querySelector('.grid')
+  grid.insertAdjacentHTML('afterbegin', htmlStr)
+}
+
+/**
+ * 等待图片加载
+ * @return {[type]} [description]
+ */
+function waitImgsLoad() {
+  var imgs = document.querySelectorAll('.grid img')
+  var totalImgs = imgs.length
+  var count = 0
+  //console.log(imgs)
+  for (var i = 0; i < totalImgs; i++) {
+    if (imgs[i].complete) {
+      //console.log('complete');
+      count++
+    } else {
+      imgs[i].onload = function() {
+        // alert('onload')
+        count++
+        //console.log('onload' + count)
+        if (count == totalImgs) {
+          //console.log('onload---bbbbbbbb')
+          initGrid()
+        }
+      }
+    }
+  }
+  if (count == totalImgs) {
+    //console.log('---bbbbbbbb')
+    initGrid()
+  }
+}
+
+/**
+ * 初始化栅格布局
+ * @return {[type]} [description]
+ */
+function initGrid() {
+  var msnry = new Masonry('.grid', {
+    // options
+    itemSelector: '.grid-item',
+    columnWidth: 250,
+    isFitWidth: true,
+    gutter: 20
+  })
+}
---
+++ page/4about.md
@@ -0,0 +1,30 @@
+---
+layout: page
+title: 关于我
+permalink: /about/
+icon: heart
+type: page
+---
+
+* content
+{:toc}
+
+暂时没想好,想好再写
+
+## 联系我
+
+* GitHub：[zxhnext](https://github.com/zxhnext)
+* email：xiaohannext@qq.com
+* [Weibo](http://weibo.com/zxhnext)
+* [知乎](https://www.zhihu.com/people/zxhnext)
+* [Facebook](https://www.facebook.com/zxhnext)
+
+
+
+## 友情链接
+
+[jekyll](http://jekyllcn.com) \| [github](https://www.github.com) \| [thinktxt](https://www.thinktxt.com) \|  [月影WEB](http://www.yydpt.com) \| 
+
+## 评论
+
+{% include comments.html %}
